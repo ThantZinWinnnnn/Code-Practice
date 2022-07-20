899f7d0733d9539e50ad4c68e48d9e40e50aad22
Thant Zin Win <thant.zin.winnnn@gmail.com>
2022-07-20 21:34:21 +0630

    class duck typing

---
+++ image/functional programming/classs/classDuckTyping.js
@@ -0,0 +1,20 @@
+class bird 
+{
+    fly()
+    {
+        console.log('bird can fly')
+    }
+}
+class aeroplane
+{
+    fly()
+    {
+        console.log('aeroplane can fly')
+    }
+}
+
+let obj = new bird();
+obj.fly();
+obj = new aeroplane();
+obj.fly();
+console.log('obj',obj)
