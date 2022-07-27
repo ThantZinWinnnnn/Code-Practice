c17336865ddca0fcb612b2539eaee0ff496cfe9b
Thant Zin Win <thant.zin.winnnn@gmail.com>
2022-07-27 14:10:40 +0630

    extra practice

--- ArrayEx/practiceArr.js
+++ ArrayEx/practiceArr.js
@@ -1,24 +1,38 @@
-let arr1 = [1, 2, 3];
-let arr2 = [100, 2, 1, 10];
+// let arr1 = [1, 2, 3];
+// let arr2 = [100, 2, 1, 10];
 
-let result = [];
-function contrast(ar,ar2)
+// let result = [];
+// function contrast(ar,ar2)
+// {
+//             for (let ele in ar) 
+//         {
+//             if(!ar2.includes(ar[ele]))
+//             {
+//               result.push(ar[ele]);
+//             }
+//         }
+//         for(let ele in ar2)
+//         {
+//             if(!ar.includes(ar2[ele]))
+//             {
+//                 result.push(ar2[ele]);
+//             }
+//         }
+//         return result.sort((a,b)=>a-b)
+// }
+
+// console.log('contrast',contrast(arr1,arr2));
+
+
+const upperFunction = (str)=>
 {
-            for (let ele in ar) 
-        {
-            if(!ar2.includes(ar[ele]))
-            {
-              result.push(ar[ele]);
-            }
-        }
-        for(let ele in ar2)
-        {
-            if(!ar.includes(ar2[ele]))
-            {
-                result.push(ar2[ele]);
-            }
-        }
-        return result.sort((a,b)=>a-b)
+    let array = str.split('');
+    console.log('arr',array)
+    let final = [];
+    for(let i = 0;i < array.length ;i ++)
+    {
+        final.push(array[i].charAt(0).toUpperCase()+array[i].slice(1));
+    }
+    return final.join('');
 }
-
-console.log('contrast',contrast(arr1,arr2));
+console.log('upper ',upperFunction('the car production'))
